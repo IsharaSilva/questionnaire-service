950130487c3e15f9365c463a5ef60d4a5e7d3930
Amila Madusanka <amila.madusanka@xitricon.com>
2023-11-13 15:27:32 +0530

    improve code quality

---
+++ src/test/java/com/xitricon/questionnaireservice/utils/TestConstants.java
@@ -0,0 +1,9 @@
+package com.xitricon.questionnaireservice.utils;
+
+public class TestConstants {
+	public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+	
+	private TestConstants() {
+		throw new IllegalStateException("Utility class");
+	}
+}
